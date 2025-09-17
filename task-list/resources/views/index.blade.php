cdc17b4928990eaae4a23abfa8b81655cb396227
OrNixz <ramadhanafif172@gmail.com>
2025-09-17 16:14:44 +0700

    feat(task): Implement show task functionality + Create a Blade layout for consistent UI. + Implement the logic in the route to find a single task by its ID. + Create the `show.blade.php` view to display task details. + Refactor `index.blade.php` to use the new Blade layout.

--- task-list/resources/views/index.blade.php
+++ task-list/resources/views/index.blade.php
@@ -1,23 +1,14 @@
-<h1>
-    The list of tasks
-</h1>
+@extends('layouts.app')
 
-<div>
-    {{-- @if (count($tasks))
-        <div>There are tasks!</div>
-        @foreach ($tasks as $task)
-            <div>{{ $task->title }}</div>
-        @endforeach
-    @else
-        <div>There are no tasks!</div>
-    @endif --}}
+@section('title', 'The list of tasks')
 
+@section('content')
     @forelse ($tasks as $task)
         <div>
-            {{-- {{ $task->title }} --}}
             <a href="{{ route('tasks.show', ['id' => $task->id]) }}">{{ $task->title }}</a>
         </div>
     @empty
         <div>There are no tasks!</div>
     @endforelse
-</div>
+
+@endsection
