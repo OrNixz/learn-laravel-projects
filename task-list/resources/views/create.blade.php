111bb3b55b444322ad487a486eaf841990963489
OrNixz <ramadhanafif172@gmail.com>
2025-09-18 17:23:47 +0700

    feat(task): Add flash messages and validation error display

--- task-list/resources/views/create.blade.php
+++ task-list/resources/views/create.blade.php
@@ -2,20 +2,44 @@
 
 @section('title', 'Create a task')
 
+@section('styles')
+<style>
+    .error-message {
+        color: red;
+        font-size: 1rem;
+    }
+</style>
+@endsection
+
 @section('content')
-    {{ $errors }}
     <form action="{{ route('tasks.store' ) }}" method="post">
         @csrf
-        <div class="">
+        <div>
             <label for="title">Title</label>
             <input type="text" name="title" id="title">
+
+            @error('title')
+                <p class="error-message">{{ $message }}</p>
+            @enderror
         </div>
 
-        <label for="description">Description</label>
-        <textarea name="description" id="description" rows="5"></textarea>
+        <div class="">
+            <label for="description">Description</label>
+            <textarea name="description" id="description" rows="5"></textarea>
 
-        <label for="long_description">Long Description</label>
-        <textarea name="long_description" id="long_description" rows="10"></textarea>
+            @error('description')
+                <p class="error-message">{{ $message }}</p>
+            @enderror
+        </div>
+
+        <div class="">
+            <label for="long_description">Long Description</label>
+            <textarea name="long_description" id="long_description" rows="10"></textarea>
+
+            @error('long_description')
+                <p class="error-message">{{ $message }}</p>
+            @enderror
+        </div>
 
         <button type="submit">Add Task </button>
     </form>
