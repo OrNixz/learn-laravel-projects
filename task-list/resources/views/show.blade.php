fb4d9393454ae2507c9f1071848e5d7aaad0b915
OrNixz <ramadhanafif172@gmail.com>
2025-09-18 14:07:12 +0700

    refactor(task): Fetch data from database using Eloquent

--- task-list/resources/views/show.blade.php
+++ task-list/resources/views/show.blade.php
@@ -3,7 +3,7 @@
 @section('title', $task->title)
 
 @section('content')
-    <p>{{ $task->description}}</p>
+    <p>{{ $task->description }}</p>
 
     @if ($task->long_description)
         <p>{{ $task->long_description }}</p>
