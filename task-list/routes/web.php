0d829e52967d623a623573fcecc357852a0cc146
OrNixz <ramadhanafif172@gmail.com>
2025-09-19 09:50:01 +0700

    feat(task): Implement delete task functionality + Add a 'destroy' route to handle task deletion. + Implement the delete logic in the route closure. + Add a delete button and form to the 'show' view. + Redirect to index with a success message on deletion.

--- task-list/routes/web.php
+++ task-list/routes/web.php
@@ -13,8 +13,6 @@ Route::get('/', function () {
 Route::get('/tasks', function () {
     return view('index', [
         'tasks' => Task::all()
-        // 'tasks' => Task::latest()->get()
-        // 'tasks' => Task::latest()->where('completed', true)->get()
     ]);
 })->name('tasks.index');
 
@@ -29,21 +27,11 @@ Route::get('/tasks/{task}/edit', function (Task $task) {
 
 Route::get('/tasks/{task}', function (Task $task) {
     return view('show', [
-        // 'task' => Task::find($id)
         'task' => $task
     ]);
 })->name('tasks.show');
 
 Route::post('/tasks', function (TaskRequest $request) {
-    // dd('We have reached the store route');
-    // dd($request->all());
-    
-    // $data = $request->validated();
-    // $task = new Task;
-    // $task->title = $data['title'];
-    // $task->description = $data['description'];
-    // $task->long_description = $data['long_description'];
-    // $task->save();
     $task = Task::create($request->validated());
 
     return redirect()->route('tasks.show', ['task' => $task->id])
@@ -51,13 +39,15 @@ Route::post('/tasks', function (TaskRequest $request) {
 })->name('tasks.store');
 
 Route::put('/tasks/{task}', function (Task $task, TaskRequest $request) {
-    // $data = $request->validated();
-    // $task->title = $data['title'];
-    // $task->description = $data['description'];
-    // $task->long_description = $data['long_description'];
-    // $task->save();
     $task->update($request->validated());
 
     return redirect()->route('tasks.show', ['task' => $task->id])
         ->with('success', 'Task updated successfully!');
 })->name('tasks.update');
+
+Route::delete('/tasks/{task}', function (Task $task) {
+    $task->delete();
+
+    return redirect()->route('tasks.index')
+        ->with('success', 'Task deleted successfully');
+})->name('tasks.destroy');
