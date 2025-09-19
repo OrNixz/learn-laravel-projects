7a3fcf29e3fb32b4a8d0306dd3747663267be00f
OrNixz <ramadhanafif172@gmail.com>
2025-09-19 11:31:44 +0700

    feat(task): Implement toggle completion status for tasks + Add a method to the Task model to toggle the 'completed' status. + Create a new PUT route to handle the toggle action. + Display the completion status on the show page. + Add a form to the show page to mark tasks as complete or incomplete.

--- task-list/resources/views/show.blade.php
+++ task-list/resources/views/show.blade.php
@@ -12,8 +12,28 @@
     <p>{{ $task->created_at }}</p>
     <p>{{ $task->updated_at }}</p>
 
+    <p>
+        @if ($task->completed)
+            Completed
+        @else
+            Not completed
+        @endif
+    </p>
+
     <div>
-        <form action="{{ route('tasks.destroy', ['task' => $task->id]) }}" method="post">
+        <a href="{{ route('tasks.edit', ['task' => $task]) }}">Edit task</a>
+    </div>
+
+    <div>
+        <form action="{{ route('tasks.toggle-completed', ['task' => $task]) }}" method="post">
+            @csrf
+            @method('PUT')
+            <button type="submit">Mark as {{ $task->completed ? 'incomplete' : 'complete' }}</button>
+        </form>
+    </div>
+
+    <div>
+        <form action="{{ route('tasks.destroy', ['task' => $task]) }}" method="post">
             @csrf
             @method('DELETE')
             <button type="submit">Delete</button>
