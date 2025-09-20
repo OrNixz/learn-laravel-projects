da1420e1d56b615f3ffe213d74a4e8c0e8a975bb
OrNixz <ramadhanafif172@gmail.com>
2025-09-20 21:29:02 +0700

    style(ui): Enhance UI and readability of task detail page + Create consistent styling for buttons and links. + Improve date presentation with human-readable formatting. + Add color coding for the task's completion status. + Organize action buttons into a clean, horizontal layout.

--- task-list/resources/views/layouts/app.blade.php
+++ task-list/resources/views/layouts/app.blade.php
@@ -6,9 +6,21 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Laravel 10 Task List App</title>
     <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+
+    {{-- blade-formatter-disable --}}
+    <style type="text/tailwindcss">
+        .btn {
+            @apply px-2 py-1 font-medium text-center rounded-md shadow-sm ring-1 ring-slate-700/10 hover:bg-slate-50 text-slate-500
+        }
+        .link {
+            @apply font-medium text-gray-700 underline decoration-pink-500
+        }
+    </style>
+    {{-- blade-formatter-enable --}}
+
     @yield('styles')
 </head>
-<body class="container mx-auto mt-10 mb-10 max-w-lg">
+<body class="container max-w-lg mx-auto mt-10 mb-10">
     <h1 class="mb-4 text-2xl">@yield('title')</h1>
     <div>
         @if (session()->has('success'))
