60eee47f8e8015d1297d2eb6914a8a9ab0c649a9
OrNixz <ramadhanafif172@gmail.com>
2025-09-20 20:04:19 +0700

    style(ui): Integrate Tailwind and use @class directive + Add the Tailwind CSS CDN script to the main layout. + Use utility classes to style the layout and index page. + Implement the @class directive for conditional styling of tasks.

--- task-list/resources/views/layouts/app.blade.php
+++ task-list/resources/views/layouts/app.blade.php
@@ -5,15 +5,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Laravel 10 Task List App</title>
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
     @yield('styles')
 </head>
-<body>
-    <h1>@yield('title')</h1>
+<body class="container mx-auto mt-10 mb-10 max-w-lg">
+    <h1 class="mb-4 text-2xl">@yield('title')</h1>
     <div>
         @if (session()->has('success'))
             <div>{{ session('success' )}}</div>
         @endif
-        
+
         @yield('content')
     </div>
 </body>
