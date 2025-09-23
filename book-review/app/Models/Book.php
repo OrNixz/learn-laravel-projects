b04febcbf05a699a19e1536612e802301740a682
OrNixz <ramadhanafif172@gmail.com>
2025-09-23 09:27:55 +0700

    feat(model): Add 'popular' and 'highestRated' scopes to Book model + Add a query scope to find the most-reviewed (popular) books. + Add a query scope to find books with the highest average rating.

--- book-review/app/Models/Book.php
+++ book-review/app/Models/Book.php
@@ -10,11 +10,25 @@ class Book extends Model
 {
     use HasFactory;
 
-    public function reviews() {
+    public function reviews()
+    {
         return $this->hasMany(Review::class);
     }
 
-    public function scopeTitle(Builder $query, string $title): Builder {
+    public function scopeTitle(Builder $query, string $title): Builder
+    {
         return $query->where('title', 'LIKE', '%' . $title . '%');
+    }
+
+    public function scopePopular(Builder $query): Builder
+    {
+        return $query->withCount('reviews')
+            ->orderBy('reviews_count', 'desc');
+    }
+
+    public function scopeHighestRated(Builder $query): Builder
+    {
+        return $query->withAvg('reviews', 'rating')
+            ->orderBy('reviews_avg_rating', 'desc');
     }
 }
