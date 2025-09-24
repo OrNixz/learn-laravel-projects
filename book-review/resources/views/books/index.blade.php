d643f9e1f88b8e53fe102c9a33e78dace6d4a98a
OrNixz <ramadhanafif172@gmail.com>
2025-09-24 20:35:42 +0700

    feat(filter): Implement backend logic for advanced filtering + Add logic to the BookController to process the 'filter' query parameter. + Create new, high-level query scopes on the Book model + Use a match expression to dynamically apply the selected filter. + Includes a minor typo fix for a filter key in the index view.

--- book-review/resources/views/books/index.blade.php
+++ book-review/resources/views/books/index.blade.php
@@ -16,8 +16,8 @@
             '' => 'Latest',
             'popular_last_month' => 'Popular Last Month',
             'popular_last_6months' => 'Popular Last 6 Months',
-            'highest_rate_last_month' => 'Highest Rated Last Month',
-            'highest_rate_last_6month' => 'Highest Rated Last 6 Months'
+            'highest_rated_last_month' => 'Highest Rated Last Month',
+            'highest_rated_last_6months' => 'Highest Rated Last 6 Months'
         ];
         @endphp
 
